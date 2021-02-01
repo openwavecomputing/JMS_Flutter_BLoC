8ca501721e4a2fe8b30f8ab0130fa551092717c0
Damu <dhamuopenwave@gmail.com>
2021-02-01 18:04:07 +0530

    Duplicated debug banner removed and yaml organized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:jms_flutter_bloc/modules/detail/job_detail.dart';
 import 'package:jms_flutter_bloc/modules/feedback/feedback.dart';
+import 'package:jms_flutter_bloc/modules/job_history/job_history.dart';
+import 'package:jms_flutter_bloc/modules/job_update/job_update.dart';
+import 'package:jms_flutter_bloc/modules/splash/splash_screen.dart';
 import 'package:jms_flutter_bloc/modules/tenant_status/tenant_others/tenant_others.dart';
 import 'package:jms_flutter_bloc/modules/to_be_continue/to_be_continue.dart';
 import 'package:jms_flutter_bloc/modules/workdone/workdone.dart';
@@ -20,10 +24,12 @@ class MyApp extends StatelessWidget {
     getValue(LoginBloc);
     return MaterialApp(
       title: 'JMS',
-      onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: FeedbackScreen(),
+      onGenerateRoute: RouteGenerator.generateRoute,
+      home: SafeArea(
+        child: Scaffold(
+          body: JobUpdate(),
+        ),
       ),
     );
   }
