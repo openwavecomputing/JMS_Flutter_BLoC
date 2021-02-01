8695d538219d041773cad1e15ba8faaf39af3bcd
logesh sharma <logeshsharmaopenwave@gmail.com>
2021-02-01 17:59:55 +0530

    - created Work done screen - Submit button align is pending.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:jms_flutter_bloc/modules/feedback/feedback.dart';
 import 'package:jms_flutter_bloc/modules/tenant_status/tenant_others/tenant_others.dart';
 import 'package:jms_flutter_bloc/modules/to_be_continue/to_be_continue.dart';
+import 'package:jms_flutter_bloc/modules/workdone/workdone.dart';
 import 'config/routes/route_generator.dart';
 import 'modules/hazard_form/hazard_form.dart';
 import 'modules/login/bloc/login_bloc.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'JMS',
       onGenerateRoute: RouteGenerator.generateRoute,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: FeedbackScreen(),
       ),
---
+++ lib/modules/workdone/workdone.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WorkDoneScreen extends StatefulWidget {
+  @override
+  _WorkDoneScreenState createState() => _WorkDoneScreenState();
+}
+
+class _WorkDoneScreenState extends State<WorkDoneScreen> {
+
+  final descriptionController = TextEditingController();
+  final locationController = TextEditingController();
+  FocusNode descriptionFocusNode;
+  FocusNode locationFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    descriptionFocusNode = FocusNode();
+    locationFocusNode = FocusNode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Work Done",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20.0,
+                  color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: ListView(
+          children: [
+            Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.black26,
+                child: Text(
+                  "Work Order : 123372345",
+                  style: TextStyle(fontSize: 18.0, color: Colors.black),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    hintText: 'Description',
+                    labelText: 'Description'),
+                maxLength: 1000,
+                controller: descriptionController,
+                cursorColor: Colors.blue,
+                focusNode: descriptionFocusNode,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.0,
+                    color: Colors.black),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.blue)),
+                      hintText: 'Location',
+                      labelText: 'Location'),
+                  maxLength: 50,
+                  controller: locationController,
+                  cursorColor: Colors.blue,
+                  focusNode: locationFocusNode,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16.0,
+                      color: Colors.black)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 175),
+              child: MaterialButton(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(8.0),
+                  ),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Submit"),
+                  //    style: TextStyle(fontSize: 14)
+                  onPressed: () {
+                    setState(() {});
+                  }),
+            )
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    descriptionController.dispose();
+    locationController.dispose();
+    descriptionFocusNode.dispose();
+    locationFocusNode.dispose();
+    super.dispose();
+  }
+}
